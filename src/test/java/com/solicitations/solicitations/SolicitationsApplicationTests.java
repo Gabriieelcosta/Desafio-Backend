c4c6299b93d7f46dd8659542ff4a7891a875692c
Gabriel Oliveira <gabrielolivecos99@gmail.com>
2026-05-08 14:32:04 -0300

    first commit

---
+++ src/test/java/com/solicitations/solicitations/SolicitationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solicitations.solicitations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolicitationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
